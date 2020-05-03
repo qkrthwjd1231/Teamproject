0a7b5ba649d809c17a5ab842c2b105f0b86fb37d
Parvaneh <qkrthwjd1231@naver.com>
2020-05-03 12:55:11 +0900

    Homeworkss

---
+++ UI과제/UI/Assets/script/name.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class name : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public InputField InputText;
+
+    public void Test(Text text)
+    {
+        text.text = InputText.text;
+    }
+    
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
---
+++ UI과제/UI/Assets/script/test.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test : MonoBehaviour
+
+{
+    [SerializeField] private Text txt_name;
+    [SerializeField] private Image img_name;
+    [SerializeField] private Slider Slider_;
+
+    private bool isCoolTime = false;
+
+    private float currentTime = 3f;
+    private float delayTime = 3f;
+
+    private void Update()
+    {
+        if (isCoolTime)
+        {
+            currentTime -= Time.deltaTime;
+            img_name.fillAmount = currentTime / delayTime;
+            img_name.fillAmount = currentTime;
+
+            if(currentTime <= 0)
+            {
+                isCoolTime = false;
+                currentTime = delayTime;
+                img_name.fillAmount = currentTime;
+            }
+        }
+    }
+
+
+    public void Change()
+
+    {
+        txt_name.text = "확인되었습니다.";
+        isCoolTime = true;
+    }
+
+    public void button()
+    {
+        Slider_.value -= 1;
+    }
+}
